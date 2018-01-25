da8173a4b590a60249d6ca948ba3be4d8c0c738c
Shinevision <ShinevisionSV@Gmail.com>
2018-01-25 20:31:18 +0100

    SpawnObject Script changed
    SpawnObject Script changed

--- Assets/SpawnRandomObjects.cs
+++ Assets/SpawnRandomObjects.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class SpawnRandomObjects : MonoBehaviour
 {
     public GameObject ground;
+    private float currentTime;
     [Space(10)]
+    public float delayTimeSec;
     public GameObject[] obstaclePrefabs;
 
     private void OnTriggerEnter(Collider other)
@@ -14,19 +16,30 @@ public class SpawnRandomObjects : MonoBehaviour
         SpawnObjects();
     }
 
+    public void Update()
+    {
+        currentTime += Time.deltaTime * delayTimeSec;
+    }
+
     void SpawnObjects()
     {
-        float maxWidht = ground.transform.localScale.x;
-        float maxLength = ground.transform.localScale.z;
 
-        float maxWidthRandom = UnityEngine.Random.Range(-maxWidht, maxWidht);
-        //float maxLenghtRandom = UnityEngine.Random.Range(-maxLength, maxLength);
+        if (currentTime >= delayTimeSec)
+        {
+            currentTime = 0;
 
-        int randomPrefab = UnityEngine.Random.Range(0, obstaclePrefabs.Length);
+            float maxWidht = ground.transform.localScale.x;
+            float maxLength = ground.transform.localScale.z;
 
-        GameObject obstacleObject;
+            float maxWidthRandom = UnityEngine.Random.Range(-10, 10);
+            //float maxLenghtRandom = UnityEngine.Random.Range(-maxLength, maxLength);
 
-        obstacleObject = Instantiate(obstaclePrefabs[randomPrefab],new Vector3(ground.transform.position.x  + maxWidthRandom,1,transform.position.z),Quaternion.identity);
+            int randomPrefab = UnityEngine.Random.Range(0, obstaclePrefabs.Length);
+
+            GameObject obstacleObject;
+
+            obstacleObject = Instantiate(obstaclePrefabs[randomPrefab], new Vector3(ground.transform.position.x + maxWidthRandom, 1, transform.position.z), Quaternion.identity);
+        }
 
     }
 }
