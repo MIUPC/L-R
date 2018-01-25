0b871814ffd6e3bc062fd02a32e75f65ef34cac2
Shinevision <ShinevisionSV@Gmail.com>
2018-01-25 19:39:57 +0100

    Added Obstacle script
    -Added Obstacle script

---
+++ Assets/L-R/Scripts/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<PlayerMovement>() != null)
+        {
+            Debug.Log("We've hit the player!");
+
+            col.gameObject.GetComponent<PlayerMovement>().enabled = false;
+            col.rigidbody.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
