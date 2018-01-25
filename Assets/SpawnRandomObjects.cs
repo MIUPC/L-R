b0a5dc83e4a63e61d31d078b16aaa50f664795c4
Shinevision <ShinevisionSV@Gmail.com>
2018-01-25 20:23:39 +0100

    Added spawning objects system

---
+++ Assets/SpawnRandomObjects.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRandomObjects : MonoBehaviour
+{
+    public GameObject ground;
+    [Space(10)]
+    public GameObject[] obstaclePrefabs;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SpawnObjects();
+    }
+
+    void SpawnObjects()
+    {
+        float maxWidht = ground.transform.localScale.x;
+        float maxLength = ground.transform.localScale.z;
+
+        float maxWidthRandom = UnityEngine.Random.Range(-maxWidht, maxWidht);
+        //float maxLenghtRandom = UnityEngine.Random.Range(-maxLength, maxLength);
+
+        int randomPrefab = UnityEngine.Random.Range(0, obstaclePrefabs.Length);
+
+        GameObject obstacleObject;
+
+        obstacleObject = Instantiate(obstaclePrefabs[randomPrefab],new Vector3(ground.transform.position.x  + maxWidthRandom,1,transform.position.z),Quaternion.identity);
+
+    }
+}
