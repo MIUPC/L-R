16a39f702b1f99e8fe827eb560ebe5adaff2c1ba
Shinevision <ShinevisionSV@Gmail.com>
2018-01-25 20:04:54 +0100

    Added infinite terrain (W.I.P)
    Added infinite terrain (W.I.P)

---
+++ Assets/SimulateInfiniteTerrain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulateInfiniteTerrain : MonoBehaviour
+{
+    public GameObject ground;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
+            ground.transform.position = new Vector3(ground.transform.position.x, ground.transform.position.y, ground.transform.position.z + (ground.transform.localScale.z / 2 - 10));
+        }
+    }
+}
