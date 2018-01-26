0c0cf4ec0562756a512a7badc207cbc8aeabc1fe
EvoIsGod <35813940+EvoIsGod@users.noreply.github.com>
2018-01-26 18:00:30 +0000

    Added
    added

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -5,10 +5,41 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 
+    public GameObject SettingsMenu;
+    public bool SettingPopup;
     public Slider volumeSlider;
     public AudioSource volumeAudio;
+
+    void Update ()
+    {
+        if (SettingPopup == true)
+        {
+            SettingsMenu.SetActive(true);
+        }
+        if (SettingPopup == false)
+        {
+            SettingsMenu.SetActive(false);
+        }
+    }
     public void VolumeController()
     {
         volumeAudio.volume = volumeSlider.value;
+    }
+
+    public void OpenSettings()
+    {
+        SettingPopup = true;
+    }
+    public void CloseSettings()
+    {
+        SettingPopup = false;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Play(string LevelName)
+    {
+        Application.LoadLevel(LevelName);
     }
 }
