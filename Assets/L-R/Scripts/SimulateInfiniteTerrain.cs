9536155962df1e82b84f7b473401241a43b76cb2
MIUPC <robinplom@gmail.com>
2018-01-27 14:06:20 +0100

    fixed not assigned error

--- Assets/L-R/Scripts/SimulateInfiniteTerrain.cs
+++ Assets/L-R/Scripts/SimulateInfiniteTerrain.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 public class SimulateInfiniteTerrain : MonoBehaviour
 {
     public GameObject ground;
-    public GameObject floor;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerMovement>() != null)
         {
             ground.transform.position = new Vector3(ground.transform.position.x, ground.transform.position.y, ground.transform.position.z + (ground.transform.localScale.z / 2 - 25));
-        }
-        if (other.GetComponent<PlayerMovement>() != null)
-        {
-            floor.transform.position = new Vector3(floor.transform.position.x, floor.transform.position.y, floor.transform.position.z + (floor.transform.localScale.z / 2 - 25));
         }
     }
 }
